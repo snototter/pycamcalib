3c897c4eb2e94cc53a9cd45823588f5f659da682
snototter <snototter@users.noreply.github.com>
2021-09-26 22:31:09 +0200

    adjust export utils

--- pcc/patterns/__init__.py
+++ pcc/patterns/__init__.py
@@ -1,6 +1,7 @@
 # Import common utils and export functionality for convenience
 from .common import *
-from .export import *
+from .export import export_board
 
 # Import specific patterns only as a submodule
 from . import eddie
+from . import dai
--- pcc/patterns/export.py
+++ pcc/patterns/export.py
@@ -1,52 +1,77 @@
 import logging
 import os
+import io
+import svgwrite
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPDF, renderPM
 from vito import pyutils
 
 
-#FIXME add export_svg flag - if false, use stringio
-def export_pattern(pattern_spec, output_folder, output_basename=None,
-                   export_pdf=True, export_png=True, prevent_overwrite=True):
-    """Saves the given pattern as SVG and optionally PDF and/or PNG.
-    
-    :pattern_spec:    The PatternSpecification... object
+def _export_svgwrite_drawing(dwg: svgwrite.Drawing, filename: str):
+    dwg.saveas(filename, pretty=True)
 
-    :output_folder:   Folder where to save the files
 
-    :output_basename: If None, the pattern_spec's slugified identifier
-                      will be used. Otherwise, the files will be stored
-                      as <basename>.<extension>
+def export_board(board_specification, output_basename: str=None, output_folder: str='.',
+                 export_pdf: bool=True, export_png: bool=True, export_svg: bool=True,
+                 prevent_overwrite: bool=True):
+    """Saves the given calibration pattern to disk.
     
-    TODO doc these flags:
-    :export_pdf:
-    :export_png:
-    :prevent_overwrite:
+:board_specification: Contains the calibration board specification.
+
+:output_basename:   If None, the board_specification's slugified identifier
+                    will be used. Otherwise, the file(s) will be stored
+                    as <basename>.<extension>
+
+:output_folder:     Folder where to save the files
+
+:export_pdf, export_png, export_svg: Flags to select the desired output
+                    format(s).
+
+:prevent_overwrite: If True and the output file(s) already exist(s), a
+                    FileExistsError will be raised
     """
+    if all([f is False for f in [export_pdf, export_png, export_svg]]):
+        raise ValueError('You must enable at least one export format!')
+
     if output_basename is None:
-        output_basename = pyutils.slugify(pattern_spec.name)
+        output_basename = pyutils.slugify(board_specification.name)
+
+    # Prevent overwrite if requested:
     fn_svg = os.path.join(output_folder, output_basename + '.svg')
     fn_pdf = os.path.join(output_folder, output_basename + '.pdf')
     fn_png = os.path.join(output_folder, output_basename + '.png')
-    # Prevent overwrite if requested:
     if prevent_overwrite:
-        for fn, tp in [(fn_svg, 'SVG'), (fn_pdf, 'PDF'), (fn_png, 'PNG')]:
+        logging.info(f'Checking existing files at output location to prevent overwriting.')
+        to_check = list()
+        if export_pdf:
+            to_check.append((fn_pdf, 'PDF'))
+        if export_png:
+            to_check.append((fn_png, 'PNG'))
+        if export_svg:
+            to_check.append((fn_svg, 'SVG'))
+        for fn, tp in to_check:
             if os.path.exists(fn):
                 raise FileExistsError(f'{tp} file already exists: {fn}')
+
     if not os.path.exists(output_folder):
         logging.info(f'Creating output folder: {output_folder}')
         os.makedirs(output_folder)
-    # First, let the pattern export itself to svg...
-    pattern_spec.export_svg(fn_svg)
-    logging.info(f'Exported {pattern_spec.name} to {fn_svg}')
-    # ... then export to png/pdf
+
+    svg_dwg = board_specification.svg()
+
+    if export_svg:
+        logging.info(f'Exporting {board_specification.name} to {fn_svg}')
+        _export_svgwrite_drawing(svg_dwg, fn_svg)
+
     if export_pdf or export_png:
-        drawing = svg2rlg(fn_svg)
+        # Parse svgwrite.Drawing into svglib
+        drawing = svg2rlg(io.StringIO(svg_dwg.tostring()))
         if export_pdf:
+            logging.info(f'Exporting {board_specification.name} to {fn_pdf}')
             renderPDF.drawToFile(drawing, fn_pdf)
-            logging.info(f'Exported {pattern_spec.name} to {fn_pdf}')
         if export_png:
-            #FIXME increasing dpi doesn't work: https://github.com/deeplook/svglib/issues/207 
-            # Maybe switch to inkscape
+            #TODO increasing dpi doesn't work: https://github.com/deeplook/svglib/issues/207 
+            # Future ideas: use inkscape (adds an external requirement, but their PNG export works
+            # perfectly)
+            logging.info(f'Exporting {board_specification.name} to {fn_png}')
             renderPM.drawToFile(drawing, fn_png, fmt="PNG")
-            logging.info(f'Exported {pattern_spec.name} to {fn_png}')
