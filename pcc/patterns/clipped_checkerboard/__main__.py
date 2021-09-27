100c924c2bd484157727aab13e590823d2abb19e
snototter <snototter@users.noreply.github.com>
2021-09-28 01:04:35 +0200

    fit demo pattern on a4

--- pcc/patterns/clipped_checkerboard/__main__.py
+++ pcc/patterns/clipped_checkerboard/__main__.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     print('img', image_points[:4,0,:])
     print('obj', object_points[0, :4, :])
 
-    board = ClippedCheckerboardSpecification('ccb-7x9', num_squares_horizontal=7,
-                                  num_squares_vertical=9,
+    board = ClippedCheckerboardSpecification('ccb-6x8', num_squares_horizontal=6,
+                                  num_squares_vertical=8,
                                   checkerboard_square_length_mm=30)
     from .. import export_board
     export_board(board, prevent_overwrite=False)
