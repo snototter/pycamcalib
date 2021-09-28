4fa72ab1439033f658dc5686b7b6226d3940d0db
snototter <snototter@users.noreply.github.com>
2021-09-28 11:55:03 +0200

    export lidar board

--- pcc/patterns/clipped_checkerboard/__main__.py
+++ pcc/patterns/clipped_checkerboard/__main__.py
@@ -1,5 +1,11 @@
 from . import *
 if __name__ == '__main__':
+    # board = ClippedCheckerboardSpecification('checkerboard', num_squares_horizontal=5,
+    #                               num_squares_vertical=9,
+    #                               checkerboard_square_length_mm=25, overlay_board_specifications=False)
+    # from .. import export_board
+    # export_board(board, prevent_overwrite=False)
+    # assert False
     board = ClippedCheckerboardSpecification('ccb-6x8', num_squares_horizontal=6,
                                   num_squares_vertical=8,
                                   checkerboard_square_length_mm=30)
@@ -13,9 +19,6 @@ if __name__ == '__main__':
     print('ref points', object_points.shape)
     print('img', image_points[:4,0,:])
     print('obj', object_points[0, :4, :])
-
-    # from .. import export_board
-    # export_board(board, prevent_overwrite=False)
 
     from vito import pyutils
     from ..imgdir import ImageDirectorySource
